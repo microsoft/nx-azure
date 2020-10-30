bf0b410fbe11d2fc11333e24dba4010c74bd4cb7
Kyle Bastien <kbastien@microsoft.com>
2020-10-30 13:26:47 -0700

    Bumping Nx + Running migrations (#12)

--- packages/storage-cache/jest.config.js
+++ packages/storage-cache/jest.config.js
@@ -2,12 +2,12 @@
 // Licensed under the MIT license.
 
 module.exports = {
-  name: "storage-cache",
-  preset: "../../jest.config.js",
+  preset: "../../jest.preset.js",
   transform: {
     "^.+\\.[tj]sx?$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
   coverageDirectory: "../../coverage/libs/storage-cache",
   globals: { "ts-jest": { tsConfig: "<rootDir>/tsconfig.spec.json" } },
+  displayName: "storage-cache",
 };
