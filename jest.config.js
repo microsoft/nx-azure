a98604931d869dc2f8cefb7c04daafea9ba01aff
Kyle Bastien <kbastien@microsoft.com>
2020-10-30 12:30:28 -0700

    Bumping Nx + Running migrations

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,5 @@
 // Licensed under the MIT license.
 
 module.exports = {
-  testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
-  transform: {
-    "^.+\\.(ts|js|html)$": "ts-jest",
-  },
-  resolver: "@nrwl/jest/plugins/resolver",
-  moduleFileExtensions: ["ts", "js", "html"],
-  coverageReporters: ["html"],
+  projects: ["<rootDir>/packages/storage-cache"],
 };
---
+++ jest.preset.js
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+const nxPreset = require("@nrwl/jest/preset");
+
+module.exports = {
+  ...nxPreset,
+  testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
+  transform: {
+    "^.+\\.(ts|js|html)$": "ts-jest",
+  },
+  resolver: "@nrwl/jest/plugins/resolver",
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageReporters: ["html"],
+};
