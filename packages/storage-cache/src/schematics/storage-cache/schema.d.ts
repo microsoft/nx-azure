ab9f8a09a468d6c34a3a65a2fdc71a4ce35d7d0f
Kyle Bastien <kbastien@microsoft.com>
2020-11-16 17:48:40 -0800

    Removing storage-cache plugin (#20)

--- packages/storage-cache/src/schematics/storage-cache/schema.d.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export interface StorageCacheSchematicSchema {
-  sasToken?: string;
-  storageAccount: string;
-  storageContainer: string;
-}
